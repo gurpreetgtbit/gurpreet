82ca37752418c0de7be6892c0834b8a36896e925
Gurpreet Singh <gus246568@gmail.com>
2017-11-30 09:42:37 +0530

    added cpp file to add

---
+++ add.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+
+    int fn,sn,i;
+    printf("the random no is %d",rand());
+    printf("enter first no");
+    scanf("%d",&fn);
+    printf("enter second no");
+    scanf("%d",&sn);
+    printf("the sum is %d",fn+sn);
+    getch();
+    return 0;
+}
